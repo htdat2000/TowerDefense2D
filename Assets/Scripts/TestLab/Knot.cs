4f542c2f12e866f1c79bd8c77dca174376b4081f
Muc Muc <tranhuutri0909@gmail.com>
2021-11-19 07:49:39 +0700

    create testscene

---
+++ Assets/Scripts/TestLab/Knot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string status = "Normal";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestLab/KnotsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnotsManager : MonoBehaviour
+{
+    public static GameObject[] KnotArray;
+    void Awake()
+    {
+        int noOfChild = transform.childCount;
+        KnotArray = new GameObject[noOfChild];
+        Debug.Log(noOfChild);
+        for(int i = 0; i < noOfChild; i++)
+        {
+            KnotArray[i] = transform.GetChild(i).gameObject;
+        }
+        Debug.Log(KnotArray[0].GetComponent<Knot>().status);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
