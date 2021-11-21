a2c7aa3d16765ef37d8b83968ec0d90c89785db6
htdat2000 <12a6.ht.dat@gmail.com>
2021-11-21 19:42:42 +0700

    Add Win/GameOver Panel
    can return home after game ended

---
+++ Assets/Scripts/GamePlay/GameEndController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndController : MonoBehaviour
+{
+  public void GoToHome()
+  {
+      SceneManager.LoadScene(0);
+  }
+}
