34e2ffd71a0ed97e2a4190a80d37bc29808c7b45
htdat2000 <12a6.ht.dat@gmail.com>
2021-11-22 09:45:35 +0700

    rearrange WaveSpawner

--- Assets/Scripts/GamePlay/WaveSpawner.cs
+++ Assets/Scripts/GamePlay/WaveSpawner.cs
@@ -51,7 +51,7 @@ public class WaveSpawner : MonoBehaviour
     {
         if (countdown <= 0f)                 //Create enemy
         {
-            ChooseEnemyToSpawn();
+            SpawnWave();
             countdown = timeBetweenWaves;
         }
         countdown -= Time.deltaTime;
@@ -64,16 +64,12 @@ public class WaveSpawner : MonoBehaviour
         }
     }
 
-    IEnumerator SpawnWave()
+    void SpawnWave()
     {
         wayNumber++;
         wavesText.text = wayNumber.ToString();
 
-        for (int i = 0; i <= 9; i++)
-        {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.5f);
-        }
+        StartCoroutine(ChooseEnemyToSpawn());
 
         if (enemyIndex == (maxEnemyTypes - 1))
         {
@@ -92,22 +88,6 @@ public class WaveSpawner : MonoBehaviour
         enemyAlives++;
     }
 
-    private int wavesCycleIndex = 0; //every 10 waves is a cycle 
-
-    void ChooseEnemyToSpawn() //choose between boss and normal monster
-    {
-        switch(wavesCycleIndex)
-        {         
-            case 9:
-                SpawnBoss(); //a boss will be spawn at the end of a cycle 
-                wavesCycleIndex = 0;
-                break;
-            default:
-                StartCoroutine(SpawnWave());
-                break;
-        }
-    }
-
     void SpawnBoss()
     {
         Debug.Log("Boss has been spawned"); //boss list will be created later
@@ -117,4 +97,25 @@ public class WaveSpawner : MonoBehaviour
             enemyIndex = 0;
         }*/
     }
+
+    private int wavesCycleIndex = 0; //every 10 waves is a cycle 
+
+    IEnumerator ChooseEnemyToSpawn() //choose between boss and normal monster
+    {
+        switch(wavesCycleIndex)
+        {         
+            case 9:
+                SpawnBoss(); //a boss will be spawn at the end of a cycle 
+                wavesCycleIndex = 0;
+                break;
+
+            default:
+                for(int i = 0; i <= 9; i++)
+                {
+                    SpawnEnemy();
+                    yield return new WaitForSeconds(0.5f);
+                }              
+                break;
+        }
+    }   
 }
