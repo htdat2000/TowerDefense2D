f38e6465601f84548fd8e676f467cd4b09dbe51d
Muc Muc <tranhuutri0909@gmail.com>
2021-12-01 16:09:27 +0700

    deadeffect

--- Assets/Scripts/GamePlay/Enemy.cs
+++ Assets/Scripts/GamePlay/Enemy.cs
@@ -14,6 +14,8 @@ public class Enemy : MonoBehaviour
     public int value;
     public float startSpeed;
     public Image healthBar;
+
+    public GameObject deadEffect;
     void Start()
     {
         health = healthRatio * SceneStats.equationValue;
@@ -42,6 +44,7 @@ public class Enemy : MonoBehaviour
     }
     public void Die()
     {
+        Instantiate(deadEffect, transform.position, transform.rotation);
         SceneStats.Money += value; 
         CheckRavenAround();
         WaveSystem.thisWaveEnemiesCount--;
