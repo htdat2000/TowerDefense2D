4d91a89468a014c709b317580ceb103e20809c06
htdat2000 <12a6.ht.dat@gmail.com>
2022-01-10 20:43:26 +0700

    CreateItemTest

---
+++ Assets/Scripts/CreateItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateItem : MonoBehaviour
+{
+    public GameEvent gameEvent; //Scriptable object data
+    public GameObject gameItem;
+    public void CreateItemPre()
+    {
+        GameObject item = Instantiate(gameItem);
+        item.GetComponent<ItemCarrier>().gameEvent = gameEvent;
+        item.GetComponent<ItemCarrier>().CallMethod();
+    }
+}
---
+++ Assets/Scripts/ItemCarrier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCarrier : MonoBehaviour
+{
+    public GameEvent gameEvent;
+
+    public void CallMethod()
+    {
+        Debug.Log("Item create success"); //Set image
+    }
+
+}
